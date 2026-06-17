c6a69da85a0b726cbb1552a3d9a222c586fa78eb
madu-montes <monteseduarda56@gmail.com>
2026-06-17 09:45:47 -0300

    feat: create metrify design system

--- metrify/src/app/app.routes.ts
+++ metrify/src/app/app.routes.ts
@@ -1,22 +1,30 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './layouts/main-layout/main-layout';
 
 export const routes: Routes = [
   {
     path: '',
     loadComponent: () =>
       import('./features/auth/pages/login/login')
-        .then(c => c.Login)
+      .then(c => c.Login)
   },
+
   {
-    path: 'dashboard',
-    loadComponent: () =>
-      import('./features/dashboard/pages/dashboard/dashboard')
-        .then(c => c.Dashboard)
-  },
-  {
-    path: 'patients',
-    loadComponent: () =>
-      import('./features/patients/pages/patients-list/patients-list')
-        .then(c => c.PatientsList)
+    path: 'app',
+    component: MainLayout,
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./features/dashboard/pages/dashboard/dashboard')
+          .then(c => c.Dashboard)
+      },
+      {
+        path: 'patients',
+        loadComponent: () =>
+          import('./features/patients/pages/patients-list/patients-list')
+          .then(c => c.PatientsList)
+      }
+    ]
   }
 ];
